edbd40910b9ba164e23ee41d0a8a6552a4636752
nobodyme <nobodydisturbme@gmail.com>
2019-06-04 00:29:45 +0530

    Changed unncessary class components into functional components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import RouterContainer from './container/RouterContainer';
 
-class App extends Component {
-	render() {
-		return <RouterContainer />;
-	}
+function App() {
+	return <RouterContainer />;
 }
 
 export default App;
--- frontend/src/container/RouterContainer.js
+++ frontend/src/container/RouterContainer.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import FetchSong from './FetchSong';
 import FetchSongDetail from './FetchSongDetail';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-class RouterContainer extends Component {
-	render() {
-		return (
-			<BrowserRouter>
-				<Route path="/" exact component={FetchSong} />
-				<Route path="/detail/:rank" component={FetchSongDetail} />
-			</BrowserRouter>
-		);
-	}
+function RouterContainer() {
+	return (
+		<BrowserRouter>
+			<Route path="/" exact component={FetchSong} />
+			<Route path="/detail/:rank" component={FetchSongDetail} />
+		</BrowserRouter>
+	);
 }
 
 export default RouterContainer;
