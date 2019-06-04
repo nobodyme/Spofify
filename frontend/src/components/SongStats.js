c423abb3370ed645f5381950534d1bdbb3bcfa1e
nobodyme <nobodydisturbme@gmail.com>
2019-06-04 13:33:24 +0530

    changed songStat logic to use map index instead of separate iterator, fixed key warning

--- frontend/src/components/SongStats.js
+++ frontend/src/components/SongStats.js
@@ -5,24 +5,26 @@ import { textFormat } from '../utils';
 
 function SongStats({ song }) {
 	const maxValue = [0, 0, 0, 1, 1, 11, 20, 10, 1, 1, 1, 1, 1, 200, 0, 10, 0];
-	let i = -1;
 
 	return (
 		<div className="songStats">
-			{Object.keys(song).map(songAttr => {
-				i++;
-				if (maxValue[i] !== 0) {
+			{Object.keys(song).map((songAttr, index) => {
+				if (maxValue[index] !== 0) {
 					return (
 						<div key={songAttr} className="songStats__item">
 							<div className="songStats__itemHeader">
 								<div className="songStats__title">{textFormat(songAttr)}</div>
 								<div className="songStats__value">{song[songAttr]}</div>
 							</div>
-							<StatBar song={song} songAttr={songAttr} maxValue={maxValue[i]} />
+							<StatBar
+								song={song}
+								songAttr={songAttr}
+								maxValue={maxValue[index]}
+							/>
 						</div>
 					);
 				} else {
-					return <></>;
+					return <React.Fragment key={songAttr} />;
 				}
 			})}
 		</div>
