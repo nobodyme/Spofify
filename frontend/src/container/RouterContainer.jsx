49e2568f687db2fe41713303f7f636b06fe817ae
nobodyme <nobodydisturbme@gmail.com>
2019-06-07 18:37:57 +0530

    added a 404 page

--- frontend/src/container/RouterContainer.jsx
+++ frontend/src/container/RouterContainer.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import FetchSong from './FetchSong';
 import FetchSongDetail from './FetchSongDetail';
-import { BrowserRouter, Route } from 'react-router-dom';
+import Indicator from '../components/Indicator';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function RouterContainer() {
 	return (
 		<BrowserRouter>
-			<Route path="/" exact component={FetchSong} />
-			<Route path="/detail/:rank" component={FetchSongDetail} />
+			<Switch>
+				<Route path="/" exact component={FetchSong} />
+				<Route path="/detail/:rank" component={FetchSongDetail} />
+				<Route
+					render={props => <Indicator {...props} text="404, Page not Found" />}
+				/>
+			</Switch>
 		</BrowserRouter>
 	);
 }
