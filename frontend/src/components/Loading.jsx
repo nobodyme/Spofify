93b7deecd4381d554afe659019cb9c7b20df044b
nobodyme <nobodydisturbme@gmail.com>
2019-06-12 20:22:37 +0530

    fixed loading component

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import '../styles/components/Loading.css';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const useStyles = makeStyles(theme => ({
+	progress: {
+		margin: theme.spacing(2)
+	}
+}));
 
 function Loading() {
-	return <div className="spinner">Loading</div>;
+	const classes = useStyles();
+
+	return (
+		<div className="spinner">
+			<CircularProgress className={classes.progress} color="white" />
+		</div>
+	);
 }
 
 export default Loading;
