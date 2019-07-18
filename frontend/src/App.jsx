dfd4ac314f5bae2b360a4bf99e815394c26b3fa2
nobodyme <nobodydisturbme@gmail.com>
2019-07-18 18:41:45 +0530

    moved router container to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import RouterContainer from './container/RouterContainer';
+import FetchSong from './container/FetchSong';
+import FetchSongDetail from './container/FetchSongDetail';
+import Indicator from './components/Indicator';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
-	return <RouterContainer />;
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" exact component={FetchSong} />
+				<Route path="/detail/:rank" component={FetchSongDetail} />
+				<Route
+					render={props => <Indicator {...props} text="404, Page not Found" />}
+				/>
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
