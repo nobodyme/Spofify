2800f9d84eb53d916786347fd91f9cdfac4a6134
nobodyme <nobodydisturbme@gmail.com>
2019-07-18 13:49:23 +0530

    removed unncessary state from songTableItem component

--- frontend/src/components/SongTableItem.jsx
+++ frontend/src/components/SongTableItem.jsx
@@ -1,43 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { millisToMinutesAndSeconds } from '../utils';
 import { withRouter } from 'react-router-dom';
 import '../styles/components/SongTableItem.css';
 import PropTypes from 'prop-types';
 
-class SongListItem extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			rank: ''
-		};
-		this.onClickHandler = this.onClickHandler.bind(this);
-	}
+function SongListItem(props) {
+	const { song, onMouseOverHandler, index, active } = props;
 
-	onClickHandler(song) {
-		this.setState({ rank: song.rank });
-		this.props.history.push({
+	const onClickHandler = song => {
+		props.history.push({
 			pathname: `/detail/${song.rank}`,
 			state: { song: song }
 		});
-	}
+	};
 
-	render() {
-		const { song, onMouseOverHandler, index } = this.props;
-
-		return (
-			<div
-				className={this.props.active ? 'songTableItem active' : 'songTableItem'}
-				onClick={() => this.onClickHandler(song)}
-				onMouseOver={() => onMouseOverHandler(index)}
-			>
-				<div className="songTableItem__name">{song.name}</div>
-				<div className="songTableItem__artists">{song.artists}</div>
-				<div className="songTableItem__duration">
-					{millisToMinutesAndSeconds(song.duration_ms)}
-				</div>
+	return (
+		<div
+			className={active ? 'songTableItem active' : 'songTableItem'}
+			onClick={() => onClickHandler(song)}
+			onMouseOver={() => onMouseOverHandler(index)}
+		>
+			<div className="songTableItem__name">{song.name}</div>
+			<div className="songTableItem__artists">{song.artists}</div>
+			<div className="songTableItem__duration">
+				{millisToMinutesAndSeconds(song.duration_ms)}
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
 SongListItem.propTypes = {
